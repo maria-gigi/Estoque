aa566f8cf7a32034613f141e56bb55619a54a80b
maria-gigi <gimariass35@gmail.com>
2026-02-20 21:25:51 -0300

    pagina

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -5,20 +5,35 @@
 package login;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
  * @author 025.007532
  */
+@WebServlet("/login")
 public class Login extends HttpServlet{
+    
+    private static final long serialVersionUID = 1L;
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException{
     
-            String usuario =  request.getParameter("users");
+            String usuario = request.getParameter("users");
+            String senha = request.getParameter("passw");
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            
+            if("admin".equals(usuario) && "1234".equals(senha)) {
+                response.sendRedirect("dashboard.html");
+                //out.println("<h2>Login realizado</h2>");
+            } else{
+                out.println("<h2>Usuário ou senha inválido</h2>");
+            }
     }
 }
